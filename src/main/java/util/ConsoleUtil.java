45e50671f33d29ec5d2ec2a088b679f17a8989fd
jjun <hjs8236@gmail.com>
2023-04-20 13:51:01 +0900

    Feat: 싱글톤 패턴 구현

--- src/main/java/util/ConsoleUtil.java
+++ src/main/java/util/ConsoleUtil.java
@@ -3,24 +3,31 @@ package main.java.util;
 import java.util.Scanner;
 
 public class ConsoleUtil {
-	private Scanner scanner;
-	
-	public ConsoleUtil() {
+	private final Scanner scanner;
+	private static ConsoleUtil consoleUtil;
+
+	private ConsoleUtil() {
 		this.scanner = new Scanner(System.in);
 	}
-	
+
 	public String getValueOf(String label) {
 		System.out.print(label + " : ");
 		String inputStr = scanner.nextLine();
-		inputStr = inputStr.trim(); 
+		inputStr = inputStr.trim();
 		return inputStr;
 	}
-	
+
 	public int getIntOf(String label) {
 		System.out.print(label + " : ");
 		String inputPrice = scanner.nextLine();
-		int intPrice = Integer.parseInt(inputPrice);
-		
-		return intPrice; 
+
+		return Integer.parseInt(inputPrice);
+	}
+
+	public static ConsoleUtil getInstance() {
+		if (consoleUtil == null) {
+			consoleUtil = new ConsoleUtil();
+		}
+		return consoleUtil;
 	}
 }
